a00e06c39d2b834e13d1d7043da46b245ca166e9
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-12-09 21:19:05 +0300

    initial commit

---
+++ FlightApp/FlightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppApp.swift
+//  FlightApp
+//
+//  Created by Игорь Тимофеев on 9.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
