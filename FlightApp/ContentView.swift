2dd877d6f92e7ad8b944825c51e34116f0a66957
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-12-09 21:34:04 +0300

    init HomeView

--- FlightApp/ContentView.swift
+++ FlightApp/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader {
+            let size = $0.size
+            let safeArea = $0.safeAreaInsets
+            HomeView(size: size, safeArea: safeArea)
+                .ignoresSafeArea(.container, edges: .vertical)
+        }
     }
 }
 
