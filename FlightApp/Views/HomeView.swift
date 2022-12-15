104724804271fc9073e071d99fc81a2d5f247d2c
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-12-15 22:44:21 +0300

    add assets

--- FlightApp/Views/HomeView.swift
+++ FlightApp/Views/HomeView.swift
@@ -14,28 +14,30 @@ struct HomeView: View {
         VStack(spacing: 0) {
             HeaderView()
         }
+        
+        @ViewBuilder
+        func HeaderView() -> some View {
+            VStack{
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: size.width * 0.4)
+                    .frame(maxWidth: .infinity, aligment: .leading)
+            }
+            .padding(15)
+            .padding(.top, safeArea.top)
+            .background {
+                Rectangle()
+                    .fill(.linearGradient(colors: [
+                        Color("BlueTop"),
+                        Color("BlueTop"),
+                        Color("BlueBotto")
+                    ], startPoint: .top, endPoint: .bottom))
+            }
+        }
     }
+}
     
-    @ViewBuilder
-    func HeaderView()->some View {
-        VStack{
-            Image("Logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: size.width * 0.4)
-                .frame(maxWidth: .infinity, aligment: .leading)
-        }
-        .padding(15)
-        .padding(.top, safeArea.top)
-        .background {
-            Rectangle()
-                .fill(.linearGradient(colors: [
-                Color("BlueTop"),
-                Color("BlueTop"),
-                Color("BlueBotto")
-                ], startPoint: .top, endPoint: .bottom))
-        }
-    }
     
     struct Home_Previews: PreviewProvider {
         static var previews: some View {
